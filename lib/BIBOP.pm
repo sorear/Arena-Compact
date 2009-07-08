c2ef57aa6f1451e7fb95ca5cf8326641a5c3f6b1
Stefan O'Rear <stefanor@cox.net>
2009-07-07 19:53:25 -0700

    Initial commit

---
+++ lib/BIBOP.pm
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+package BIBOP;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+BIBOP - ???
+
+=head1 SYNOPSIS
+
+    use BIBOP;
+
+=head1 DESCRIPTION
+
+
+
+=head1 AUTHOR
+
+Stefan O'Rear, C<< <stefanor@cox.net> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-bibop at rt.cpan.org>, or browse
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=BIBOP>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Stefan O'Rear.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
